38a2b92c4dd05fc3c78800166607bbde60ab0903
AlexArciszewski <arciszew@yahoo.com>
2024-06-04 11:54:01 +0200

    CRUD completed finally repaired task error

--- crm/urls.py
+++ crm/urls.py
@@ -7,13 +7,18 @@ urlpatterns = [
     
     path('', views.homepage, name=""),
     
-    path('task', views.task, name="task"),
+    path('view-tasks', views.tasks, name="view-tasks"),
     
     path('register/', views.register, name="register"),
     
     path('create-task', views.create_task, name="create-task"),
     
+    path('update-task/<str:pk>', views.update_task, name="update-task"),
+    
+    path('delete-task/<str:pk>', views.delete_task, name="delete-task"),
+    
  #importujemy funkcje register z views.py  pod routem "register"
  
 ]
+
 
--- crm/views.py
+++ crm/views.py
@@ -8,23 +8,58 @@ from .models import Task
 
 from .forms import TaskForm
 
+#homepage/Firt page of webside
 
 def homepage(request):
 
     """Tworzę  stronę startową"""
     return render(request, 'crm/index.html')
 
-def task(request):
+
+#CRUD -READ TASKs
+
+def tasks(request):
     
     queryDataAll = Task.objects.all()
     
     context = {'AllTasks':queryDataAll}
     
-    return render(request, 'crm/task.html', context)
+    return render(request, 'crm/view-tasks.html', context)
+
+
+
+def update_task(request, pk):
     
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+    
+    if request.method == 'POST':
+        
+        form = TaskForm(request.POST, instance=task)
+        
+        if form.is_valid():
+            
+            form.save()
+            
+            return redirect('view-tasks')
+    
+    context = {'UpdateTask': form}
+    
+    return render(request, 'crm/update-task.html',context)     
+
+
+
+
+#Registration Page    
 def register(request):
     """dodajemy metodę zwracającą response o stronie rejestracji i taki będzie wyglad strony co w return"""
     return render(request, 'crm/register.html')
+
+
+
+#CRUD Create tasks
+
 
 def create_task(request):
     form = TaskForm()
@@ -37,7 +72,7 @@ def create_task(request):
             
             form.save()
             
-            return redirect('task')
+            return redirect('view-tasks')
     
     
     context = {'TaskForm': form}
@@ -45,4 +80,22 @@ def create_task(request):
     return render(request, 'create-task.html', context)
     #outputujemy formularze z wszystkimi polami ajkie tu mamy title i description data jest wbudowana w django
     
+    
+# CRUD Delete tasks
+
+
+def delete_task(request,pk):
+    #pk to nasz pojemnik na dane
+    task = Task.objects.get(id=pk)
+    #jeśli jest zgodonosc z id z id z bazy to kasujemy
+    #patrzymy na delete-task.html tam jest metoda POST to musimy dać if 
+    if request.method == 'POST':
+        
+        task.delete()
+        
+        return redirect('view-tasks')
+        #przekierowanie do strony z taskami
+    
+    return render(request, 'crm/delete-task.html')
+#usuwam obiekt z dynamicznego urls
     
